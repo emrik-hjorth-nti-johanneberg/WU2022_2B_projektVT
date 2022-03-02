6df5c20548d718b18eb3174cd1605e7f4556c213
emrik-hjorth-nti-johanneberg <95085151+emrik-hjorth-nti-johanneberg@users.noreply.github.com>
2022-03-02 13:41:14 +0100

    Kanske har fixat buggen.
    Har kanske fixat buggen samt skrivit javascript så att cardwrappern tar upp olika mycket av skärmen beroende på om nav:en är framme eller inte.

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const burger = document.querySelector('.burger')
+const nav = document.querySelector('nav')
+const gridwrap = document.querySelector('.gridwrap')
+
+function toggleMenu() {
+    nav.classList.toggle('nav-active')
+    burger.classList.toggle('toggle')
+    gridwrap.classList.toggle("gridtoggle")
+}
+
+burger.addEventListener('click', toggleMenu)
+ 
